bb6883512d583fc548066d71c4623e4301ed61f4
Rommel <rommel.xana@gmail.com>
2025-04-08 14:10:53 +0200

    estado global localizacion

--- src/components/containers/containerSuperior/ContainerSuperior.tsx
+++ src/components/containers/containerSuperior/ContainerSuperior.tsx
@@ -9,6 +9,7 @@ import ContainerFichaje2 from './containers/ContainerFichaje2';
 
 export default function ContainerSuperior({ user }: { user: User }) {
     const [estado, setEstado] = useState('');
+    const [localizacion, setLocalizacion] = useState('');
 
     const supabase = createClient();
 
@@ -21,7 +22,7 @@ export default function ContainerSuperior({ user }: { user: User }) {
 
             const { data, error } = await supabase
                 .from('historialFichajes')
-                .select('estado')
+                .select('estado, localizacionFichaje')
                 .eq('created_at', `${year}-${mounth}-${day}`)
                 .eq('user_id', user.id);
 
@@ -31,7 +32,8 @@ export default function ContainerSuperior({ user }: { user: User }) {
             }
 
             if (data && data.length > 0) {
-                setEstado(data[0].estado)
+                setEstado(data[0].estado);
+                setLocalizacion(data[0].localizacionFichaje);
             } else {
                 console.log('undefined')
             };
@@ -42,7 +44,7 @@ export default function ContainerSuperior({ user }: { user: User }) {
 
     return (
         <div className={styles.containerSuperior}>
-            <ContainerDatos2 user={user} estado={estado}/>
+            <ContainerDatos2 user={user} estado={estado} localizacion={localizacion} setLocalizacion={setLocalizacion}/>
             <ContainerFichaje2 user={user} estado={estado} setEstado={setEstado}/>
         </div>
     );
