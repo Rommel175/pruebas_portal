50412e19e507417f1e37f8811def3ac9490f386c
Rommel <rommel.xana@gmail.com>
2025-04-08 15:07:59 +0200

    persistencia temporal rimer

--- src/app/dashboard/archivos/page.tsx
+++ src/app/dashboard/archivos/page.tsx
@@ -1,8 +1,26 @@
+import ContainerSuperior from "@/components/containers/containerSuperior/ContainerSuperior";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function FilesPage() {
+export default async function FilesPage() {
+  const supabase = await createClient();
+  const { data, error } = await supabase.auth.getUser();
+
+  if (error) {
+    redirect('/login')
+  }
+
+  const user = data.user;
+
   return (
-    <div>
-      <h1>Files page</h1>
-    </div>
+    <>
+      <div>
+        <h1>Files page</h1>
+      </div>
+
+      <div style={{display: 'none'}}>
+        <ContainerSuperior user={user}/>
+      </div>
+    </>
   );
 }
--- src/app/dashboard/recursos/layout.tsx
+++ src/app/dashboard/recursos/layout.tsx
@@ -1,11 +1,29 @@
 import NavbarRecursos from '@/components/recursos/NavbarRecursos';
 import styles from './layput.module.css'
+import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation';
+import ContainerSuperior from '@/components/containers/containerSuperior/ContainerSuperior';
 
-export default function RecursosLayout({ children }: { children: React.ReactNode; }) {
+export default async function RecursosLayout({ children }: { children: React.ReactNode; }) {
+    const supabase = await createClient();
+    const { data, error } = await supabase.auth.getUser();
+
+    if (error) {
+        redirect('/login')
+    }
+
+    const user = data.user;
+
+
+
     return (
         <div className={styles.wraper}>
             <NavbarRecursos />
-            { children }
+            <div style={{display: 'none'}}>
+                <ContainerSuperior user={user}/>
+            </div>
+            
+            {children}
         </div>
     );
 }
